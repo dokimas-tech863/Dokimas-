2fca696b722e18ecb29cb183971077d8a2c24a59
dokimas-tech863 <dokimasfkir@gmail.com>
2026-07-14 07:50:36 +0300

    Refactor bot to use asyncio for main execution
    Refactor bot to use asyncio for main execution and event handling.

--- bot.py
+++ bot.py
@@ -1,21 +1,29 @@
 # (©)Codexbotz
 import os
+import asyncio
 from pyrogram import Client
+
 
 API_ID = int(os.environ.get("API_ID", 37063855))
 API_HASH = os.environ.get("API_HASH", "0419e8626a4ac694ff59f0953c7d089e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8237959420:AAHE0atddUSOCosRnwWxqPNSB6E8We2xjz4")
 
-app = Client(
-    "my_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+async def main():
+    app = Client(
+        "my_bot",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN
+    )
+    
+    @app.on_message()
+    async def hello(client, message):
+        await message.reply("ቦቱ እየሰራ ነው!")
+    
+    print("ቦቱ ተነስቷል!")
+    await app.start()
+    # ቦቱ እንዳይዘጋ ለዘላለም እንዲቆይ
+    await asyncio.Event().wait()
 
-@app.on_message()
-async def hello(client, message):
-    await message.reply("ቦቱ እየሰራ ነው!")
-
-print("ቦቱ ተነስቷል!")
-app.run()
+if __name__ == "__main__":
+    asyncio.run(main())
